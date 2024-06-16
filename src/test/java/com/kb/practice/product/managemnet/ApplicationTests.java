75d2d34814da673f9c01ddfe9f58f7a95fd1bfb6
고운 정 <chocopie_fine@kyobobook.com>
2024-06-16 12:22:02 +0900

    project: init project

---
+++ src/test/java/com/kb/practice/product/managemnet/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kb.practice.product.managemnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
